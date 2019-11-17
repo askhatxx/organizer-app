c6c1cd4204e81804e7044ba2c4e47b71eb22184d
askhatxx <55058367+askhatxx@users.noreply.github.com>
2019-11-18 00:27:42 +0600

    Commit 7

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import Notes from './Notes';
 import Clock from './Clock';
 
+const notesTemp = [
+    {title:'Title 1', text:'Text 1'}, 
+    {title:'Title Title 2', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}, 
+    {title:'Title Title Title3', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}, 
+    {title:'Title Title 4', text:'Text Title Title Title Title 4'}, 
+    {title:'Title Title 5', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}, 
+    {title:'Title 6', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
+];
+
 export default class App extends Component {
     constructor(props) {
         super(props);
@@ -21,34 +30,54 @@ export default class App extends Component {
         const data = localStorage.getItem('clock') ? JSON.parse(localStorage.getItem('clock')) : [initialClock];
 
         this.state = {
+            notes: notesTemp,
             clock: data
         }
     }
 
+    addNote = (note) => {
+        const newNote = [note, ...this.state.notes];
+        this.updateState({notes: newNote});
+    }
+    
+    removeNote = (index) => {
+        const newNotes = this.state.notes.filter((_, i) => {
+            return index !== i;
+        });
+        this.updateState({notes: newNotes});
+    }
+
     addClock = (clock) => {
         const newClock = [...this.state.clock, clock];
-        this.updateState(newClock);
+        this.updateState({clock: newClock});
     }
 
     removeClock = (index) => {
         const newClock = this.state.clock.filter((_, i) => {
             return index !== i;
         });
-        this.updateState(newClock);
+        this.updateState({clock: newClock});
     }
 
     updateState(data) {
-        localStorage.setItem('clock', JSON.stringify(data));
-        this.setState({clock: data});
+        for (const key in data) {
+            localStorage.setItem(key, JSON.stringify(data[key]));
+        }
+        this.setState(data);
     }
     
     render() {
-        const {clock} = this.state;
+        const {notes, clock} = this.state;
         console.log(localStorage.getItem('clock'));
+        console.log(this.state);
         return (
             <div className='box'>
                 <div className='box-1'>
-                    <Notes notes={[{title:'Title 1', text:'Text 1'}, {title:'Title 2', text:'Text 2'}]}/>
+                    <Notes 
+                        notes={notes} 
+                        addNote={this.addNote}
+                        removeNote={this.removeNote}
+                    />
                 </div>
                 <div className='box-2'>
                     <Clock 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,27 +1,96 @@
 import React, {Component} from 'react';
 
 export default class Notes extends Component {
+    state = {
+        formTitle: '',
+        formText: '',
+        showForm: false
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addNote({title: this.state.formTitle, text: this.state.formText});
+        this.setState({
+            formTitle: '',
+            formText: '',
+            showForm: false
+        });
+    }
+    
+    renderForm() {
+        return (
+            <div className='box-notes-add'>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        name='formTitle'
+                        type='text'
+                        placeholder='Title'
+                        value={this.state.formTitle}
+                        onChange={this.handleChange}
+                    />
+                    <textarea
+                        name='formText'
+                        placeholder='Text'
+                        value={this.state.formText}
+                        onChange={this.handleChange}
+                    />
+                    <button
+                        type='submit'
+                    >
+                        Send
+                    </button>
+                </form>
+            </div>
+        )
+    }
+    
+    renderNoteTitleOptions(index) {
+        return <span onClick={() => this.props.removeNote(index)}>del</span>
+    }
+    
     renderNotes(notes) {
         const notesList = notes.map((item, index) => {
             const {title, text} = item;
+            const options = this.renderNoteTitleOptions(index);
+            let clazz = 'note';
+
+            if (title.length > 200 || text.length > 200) {
+                clazz += ' long';
+            }
 
             return (
-                <div key={index}>
-                    <div>{title}</div>
-                    <div>{text}</div>
+                <div key={index} className={clazz}>
+                    <div className='note-inner'>
+                        <div className='note-title'>{title} {options}</div>
+                        <div className='note-text'>{text}</div>
+                    </div>
                 </div>
             )
         });
 
         return notesList;
     }
+
+    toggleForm = () => {
+        this.setState({
+            showForm: !this.state.showForm
+        })
+    }
     
     render() {
         const notes = this.renderNotes(this.props.notes);
+        const form = this.state.showForm ? this.renderForm() : null;
         return (
             <div className='box-notes'>
-                <div className='box-notes-options'>Add</div>
+                <div className='box-notes-options'><span onClick={this.toggleForm}>Add</span></div>
                 <div className='box-notes-list'>{notes}</div>
+                {form}
             </div>
         )
     }
